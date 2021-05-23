92f68e3da16dd92546d5114f870799124c32071c
Mansoor Akhtar <mansoorakhtaricp@gmail.com>
2021-05-23 05:17:52 +0500

    proxycrawl amazon product

--- helpers/globals.js
+++ helpers/globals.js
@@ -2,12 +2,12 @@
  * Global helpers
  */
 
-global.rootPath = __dirname.replace('/helpers', '');
+global.rootPath = __dirname.replace("helpers", "");
 
 /**
  * Requires a file from root
  * @param  {string} name
  */
 global.rootRequire = (name) => {
-  return require(global.rootPath + '/' + name);
+  return require(global.rootPath + "/" + name);
 };
